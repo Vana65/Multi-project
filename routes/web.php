bdd2a64f4f3a4ce895c220dcb8e974f5fcdb889b
vana <vanazakaria73@gmail.com>
2025-10-30 01:56:43 +0300

    Customizing admin

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
 use App\Http\Controllers\FrontHomeController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Back\BackHomeController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +19,22 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('front')->name('front.')->group(function(){
         Route::get('/', FrontHomeController::class)->middleware('auth')->name('index');
         
-Route::view('/login','front.auth.login');
-                Route::view('/register','front.auth.register');
-                                Route::view('/forget-password','front.auth.forget-password');
+//Route::view('/login','front.auth.login');
+          //      Route::view('/register','front.auth.register');
+              //                  Route::view('/forget-password','front.auth.forget-password');
 
 
 });
+Route::prefix('back')->name('back.')->group(function() {
+    Route::get('/', BackHomeController::class)
+        ->name('index');
+        Route::view('/login','back.auth.login');
+              Route::view('/register','back.auth.register');
+                             Route::view('/forget-password','back.auth.forget-password');
+
+
+});
+        
 
 
 require __DIR__.'/auth.php';
