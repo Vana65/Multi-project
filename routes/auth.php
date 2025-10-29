ab93d43b17b260800172be333b1652feed66287b
vana <vanazakaria73@gmail.com>
2025-10-29 19:46:23 +0300

    Customizing email Verification

--- routes/auth.php
+++ routes/auth.php
@@ -44,7 +44,7 @@ Route::middleware('auth')->group(function () {
                 ->name('verification.verify');
 
     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-                ->middleware('throttle:6,1')
+                ->middleware('t hrottle:6,1')
                 ->name('verification.send');
 
     Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
